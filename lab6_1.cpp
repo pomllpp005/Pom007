23f5813cf702d16ba2c5032d82615309b0bcf684
unknown <pomllpp005@gmail.com>
2024-12-02 21:48:08 +0700

    compleat

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main(){
+    int integer[]={};
+    int i=0;
+    int x;
+    int y=0;
+    while(x != 0){
     cout << "Enter an integer: ";
+    cin >> x;
+    integer[i]=x;
+    // cout << integer[i]<<endl;
+    i++;
+    }
+    //cout<< "ppppppppppppppppppp"<<endl;
+    // cout<< integer[0]<<endl;
+    //  cout<< integer[1]<<endl;
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+// cout << integer[i]<<endl;
