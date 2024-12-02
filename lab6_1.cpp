4b5acbbe673a5a2b67f8e90091abeca41151621a
unknown <pomllpp005@gmail.com>
2024-12-02 22:11:56 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -10,12 +10,10 @@ int main(){
     cout << "Enter an integer: ";
     cin >> x;
     integer[i]=x;
-    // cout << integer[i]<<endl;
+  
     i++;
     }
-    //cout<< "ppppppppppppppppppp"<<endl;
-    // cout<< integer[0]<<endl;
-    //  cout<< integer[1]<<endl;
+   
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
